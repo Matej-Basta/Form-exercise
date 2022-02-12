1725703d8da728a29834a483191a7157424a0e88
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-12 11:43:26 +0100

    data inserting into a database

---
+++ edit.php
@@ -0,0 +1,38 @@
+<?php
+require_once "DBBlackbox.php";
+
+var_dump($_POST);
+
+$id = insert($_POST);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <input type="text" name="title" placeholder="title"><br>   
+        <input type="number" name="year" placeholder="year of release"><br>   
+        <select name="genre">
+            <option value="comedy">Comedy</option>
+            <option value="drama">Drama</option>
+            <option value="action">Action</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+        </select><br>   
+        <label for="released">Is Released?</label>
+        <input type="hidden" name="released" value="false">
+        <input id="released" type="checkbox" name="released" value="true"><br>   
+        <textarea name="plot" placeholder="plot" cols="30" rows="10"></textarea><br>   
+        <input type="number" name="rating" placeholder="rating"><br>   
+        <button type="Submit">Submit</button><br>   
+    </form>
+</body>
+</html>
