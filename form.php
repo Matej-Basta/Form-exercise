781cf318c6329916bdb2925ff65f3171134d9d83
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-13 21:07:49 +0100

    next step

--- form.php
+++ form.php
@@ -1,3 +1,21 @@
+<?php
+require_once "DBBlackbox.php";
+$id = null;
+$selected = null;
+$id = isset($_GET["id"]) ?? null;
+$data_retrieved = false;
+
+$selected = find($id);
+
+
+if(!empty($id)) {
+    $data_retrieved = true;
+}
+
+var_dump($selected);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +26,7 @@
 </head>
 <body>
     <form action="request-handler.php" method="POST">
-        <input type="text" name="title" placeholder="title"><br>   
+        <input type="text" name="title" placeholder="title" <?php echo $data_retrieved ? "value={$selected['title']}" : "value=''" ?> ><br>   
         <input type="number" name="year" placeholder="year of release"><br>   
         <select name="genre">
             <option value="comedy">Comedy</option>
@@ -25,5 +43,10 @@
         <input type="number" name="rating" placeholder="rating"><br>   
         <button type="Submit">Submit</button><br>   
     </form>
+    <p>Which item you want to edit?</p>
+    <form action="" method="GET">
+        <input type="number" name="id" placehodler="id">
+        <button type="Submit">Find</button>
+</form>
 </body>
 </html>
--- success.php
+++ success.php
@@ -6,5 +6,4 @@ echo "<h1>Successfuly submitted</h1>";
 
 echo "<p>You have submitted {$_SESSION['movie']['title']} with a rating of {$_SESSION['movie']['rating']}</p>";
 
-
 ?>
